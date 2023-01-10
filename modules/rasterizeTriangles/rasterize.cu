907970405d6cbba4c718941b5540b58336ed8776
Markus Schütz <markus_schuetz@gmx.at>
2023-01-10 17:54:04 +0100

    ...

--- modules/rasterizeTriangles/rasterize.cu
+++ modules/rasterizeTriangles/rasterize.cu
@@ -9,6 +9,7 @@
 namespace cg = cooperative_groups;
 
 Uniforms uniforms;
+Allocator* allocator;
 
 constexpr float PI = 3.1415;
 
@@ -33,7 +34,7 @@ struct Points{
 	unsigned int instanceCount;
 	unsigned int first;
 	unsigned int baseInstance;
-	Point* points;
+	Point points[10'000'000];
 };
 
 struct Lines{
@@ -41,7 +42,7 @@ struct Lines{
 	unsigned int instanceCount;
 	unsigned int first;
 	unsigned int baseInstance;
-	Point* vertices;
+	Point vertices[10'000'000];
 };
 
 struct Triangles{
@@ -49,17 +50,30 @@ struct Triangles{
 	unsigned int instanceCount;
 	unsigned int first;
 	unsigned int baseInstance;
-	Point* vertices;
+	float3* positions;
+	float2* uvs;
+	uint32_t* colors;
 };
 
-void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
+struct Texture{
+	int width;
+	int height;
+	uint32_t* data;
+};
+
+void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer, Texture* texture = nullptr){
 
 	auto grid = cg::this_grid();
 	auto block = cg::this_thread_block();
-	
-	// if(false)
+
+	uint32_t& processedTriangles = *allocator->alloc<uint32_t*>(4);
+	if(grid.thread_rank() == 0){
+		processedTriangles = 0;
+	}
+	grid.sync();
+
 	{
-		__shared__ int shared_triangleIndex[1];
+		__shared__ int sh_triangleIndex;
 
 		block.sync();
 
@@ -68,15 +82,15 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 			
 			block.sync();
 			if(block.thread_rank() == 0){
-				shared_triangleIndex[0] = atomicAdd(&triangles->count, -3) / 3;
+				sh_triangleIndex = atomicAdd(&processedTriangles, 1);
 			}
 			block.sync();
 
-			int triangleIndex = shared_triangleIndex[0];
+			if(sh_triangleIndex >= triangles->count / 3) break;
 
-			if(triangleIndex < 0) break;
-
-			auto toScreenCoord = [&](Point p){
+			// project x/y to pixel coords
+			// z is whatever, w is the linear depth
+			auto toScreenCoord = [&](float3 p){
 				float4 pos = uniforms.transform * float4{p.x, p.y, p.z, 1.0f};
 
 				pos.x = pos.x / pos.w;
@@ -92,9 +106,12 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 				return imgPos;
 			};
 
-			Point v0 = triangles->vertices[3 * triangleIndex + 0];
-			Point v1 = triangles->vertices[3 * triangleIndex + 1];
-			Point v2 = triangles->vertices[3 * triangleIndex + 2];
+			int i0 = 3 * sh_triangleIndex + 0;
+			int i1 = 3 * sh_triangleIndex + 1;
+			int i2 = 3 * sh_triangleIndex + 2;
+			float3 v0 = triangles->positions[i0];
+			float3 v1 = triangles->positions[i1];
+			float3 v2 = triangles->positions[i2];
 
 			float4 p0 = toScreenCoord(v0);
 			float4 p1 = toScreenCoord(v1);
@@ -116,9 +133,10 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 
 			{// backface culling
 				float w = cross(v01, v02);
-				if(w > 0.0) continue;
+				if(w < 0.0) continue;
 			}
 
+			// compute screen-space bounding rectangle
 			float min_x = min(min(p0.x, p1.x), p2.x);
 			float min_y = min(min(p0.y, p1.y), p2.y);
 			float max_x = max(max(p0.x, p1.x), p2.x);
@@ -127,10 +145,12 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 			int size_y = ceil(max_y) - floor(min_y);
 			int numFragments = size_x * size_y;
 
+			// iterate through fragments in bounding rectangle and draw if within triangle
 			int numProcessedSamples = 0;
 			for(int fragOffset = 0; fragOffset < numFragments; fragOffset += block.num_threads()){
 
-				if(numProcessedSamples > 1'000) break;
+				// safety mechanism: don't draw more than 1k pixels per triangle
+				if(numProcessedSamples > 10'000) break;
 
 				int fragID = fragOffset + block.thread_rank();
 				int fragX = fragID % size_x;
@@ -144,6 +164,9 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 
 				float s = cross(sample, v02) / cross(v01, v02);
 				float t = cross(v01, sample) / cross(v01, v02);
+				float v = 1.0 - (s + t);
+
+				// v: vertex[0], s: vertex[1], t: vertex[2]
 
 				int2 pixelCoords = make_int2(pFrag.x, pFrag.y);
 				int pixelID = pixelCoords.x + pixelCoords.y * uniforms.width;
@@ -153,17 +176,62 @@ void rasterizeTriangles(Triangles* triangles, uint64_t* framebuffer){
 
 				if( (s >= 0.0) && (t >= 0.0) && (s + t <= 1.0) )
 				{
-					
-					unsigned int depth = *((int*)&p0.w);
-					uint64_t pixel = (((unsigned long long int)depth) << 32) | v0.color;
 
-					// uint64_t pixel = v0.color;
+					uint8_t* v0_rgba = (uint8_t*)&triangles->colors[i0];
+					uint8_t* v1_rgba = (uint8_t*)&triangles->colors[i1];
+					uint8_t* v2_rgba = (uint8_t*)&triangles->colors[i2];
+
+					float2 v0_uv = triangles->uvs[i0];
+					float2 v1_uv = triangles->uvs[i1];
+					float2 v2_uv = triangles->uvs[i2];
+					float2 uv = {
+						v * v0_uv.x + s * v1_uv.x + t * v2_uv.x,
+						v * v0_uv.y + s * v1_uv.y + t * v2_uv.y
+					};
+
+					uint32_t color;
+					uint8_t* rgb = (uint8_t*)&color;
+
+					// { // color by vertex color
+					// 	rgb[0] = v * v0_rgba[0] + s * v1_rgba[0] + t * v2_rgba[0];
+					// 	rgb[1] = v * v0_rgba[1] + s * v1_rgba[1] + t * v2_rgba[1];
+					// 	rgb[2] = v * v0_rgba[2] + s * v1_rgba[2] + t * v2_rgba[2];
+					// }
+
+					// { // color by uv
+					// 	rgb[0] = 255.0f * uv.x;
+					// 	rgb[1] = 255.0f * uv.y;
+					// 	rgb[2] = 0;
+					// 	rgb[3] = 255;
+					// }
+
+					if(texture)
+					{ // color by texture
+						int tx = int(uv.x * texture->width) % texture->width;
+						int ty = int(uv.y * texture->height) % texture->height;
+						ty = texture->height - ty;
+
+						int texelIndex = tx + texture->width * ty;
+						uint32_t texel = texture->data[texelIndex];
+						uint8_t* texel_rgb = (uint8_t*)&texel;
+
+						rgb[0] = texel_rgb[0];
+						rgb[1] = texel_rgb[1];
+						rgb[2] = texel_rgb[2];
+					}else{
+						rgb[0] = v * v0_rgba[0] + s * v1_rgba[0] + t * v2_rgba[0];
+						rgb[1] = v * v0_rgba[1] + s * v1_rgba[1] + t * v2_rgba[1];
+						rgb[2] = v * v0_rgba[2] + s * v1_rgba[2] + t * v2_rgba[2];
+					}
+
+					float depth = v * p0.w + s * p1.w + t * p2.w;
+					uint64_t udepth = *((uint32_t*)&depth);
+					uint64_t pixel = (udepth << 32ull) | color;
 
 					atomicMin(&framebuffer[pixelID], pixel);
 				}
 
 				numProcessedSamples++;
-
 			}
 
 
@@ -175,18 +243,28 @@ extern "C" __global__
 void kernel(
 	const Uniforms _uniforms,
 	unsigned int* buffer,
-	cudaSurfaceObject_t gl_colorbuffer
+	cudaSurfaceObject_t gl_colorbuffer,
+	uint32_t numTriangles,
+	float3* positions,
+	float2* uvs,
+	uint32_t* colors,
+	uint32_t* textureData
 ){
 	auto grid = cg::this_grid();
 	auto block = cg::this_thread_block();
 
 	uniforms = _uniforms;
 
-	Allocator allocator(buffer, 0);
+	Allocator _allocator(buffer, 0);
+	allocator = &_allocator;
+
+	// if(grid.thread_rank() == 0){
+	// 	printf("%i \n", numTriangles);
+	// }
 
 	// allocate framebuffer memory
 	int framebufferSize = int(uniforms.width) * int(uniforms.height) * sizeof(uint64_t);
-	uint64_t* framebuffer = allocator.alloc<uint64_t*>(framebufferSize);
+	uint64_t* framebuffer = allocator->alloc<uint64_t*>(framebufferSize);
 
 	// clear framebuffer
 	processRange(0, uniforms.width * uniforms.height, [&](int pixelIndex){
@@ -197,114 +275,71 @@ void kernel(
 
 	grid.sync();
 
-	// draw plane
-	int cells = 2000;
-	processRange(0, cells * cells, [&](int index){
-		int ux = index % cells;
-		int uy = index / cells;
-
-		float u = float(ux) / float(cells - 1);
-		float v = float(uy) / float(cells - 1);
-
-		float4 pos = {
-			5.0 * (u - 0.5), 
-			5.0 * (v - 0.5), 
-			0.0f, 
-			1.0f};
-
-		float4 ndc = uniforms.transform * pos;
-		ndc.x = ndc.x / ndc.w;
-		ndc.y = ndc.y / ndc.w;
-		ndc.z = ndc.z / ndc.w;
-		float depth = ndc.w;
-
-		int x = (ndc.x * 0.5 + 0.5) * uniforms.width;
-		int y = (ndc.y * 0.5 + 0.5) * uniforms.height;
-
-		uint32_t R = 255.0f * u;
-		uint32_t G = 255.0f * v;
-		uint32_t B = 0;
-		uint64_t color = R | (G << 8) | (B << 16);
-
-		if(x > 1 && x < uniforms.width  - 2.0)
-		if(y > 1 && y < uniforms.height - 2.0){
-
-			// SINGLE PIXEL
-			uint32_t pixelID = x + int(uniforms.width) * y;
-			uint64_t udepth = *((uint32_t*)&depth);
-			uint64_t encoded = (udepth << 32) | color;
-
-			atomicMin(&framebuffer[pixelID], encoded);
-
-			// POINT SPRITE
-			// for(int ox : {-2, -1, 0, 1, 2})
-			// for(int oy : {-2, -1, 0, 1, 2}){
-			// 	uint32_t pixelID = (x + ox) + int(uniforms.width) * (y + oy);
-			// 	uint64_t udepth = *((uint32_t*)&depth);
-			// 	uint64_t encoded = (udepth << 32) | color;
-
-			// 	atomicMin(&framebuffer[pixelID], encoded);
-			// }
-		}
-	});
-
-	// draw sphere
-	int s = 10'000;
-	float rounds = 20.0;
-	processRange(0, s, [&](int index){
-		float u = float(index) / float(s - 1);
-
-		float z = 2.0 * u - 1.0;
-		float a = cos(0.5 * PI * z);
-		a = sqrt(1.0f - abs(z * z));
-
-		float r = 0.5;
-
-		float4 pos = {
-			r * a * sin(rounds * PI * u), 
-			r * a * cos(rounds * PI * u), 
-			r * z + 0.3, 
-			1.0f};
-
-		float4 ndc = uniforms.transform * pos;
-		ndc.x = ndc.x / ndc.w;
-		ndc.y = ndc.y / ndc.w;
-		ndc.z = ndc.z / ndc.w;
-		float depth = ndc.w;
-
-		int x = (ndc.x * 0.5 + 0.5) * uniforms.width;
-		int y = (ndc.y * 0.5 + 0.5) * uniforms.height;
-
-		uint32_t R = 255.0f * u;
-		uint32_t G = 0;
-		uint32_t B = 0;
-		uint64_t color = R | (G << 8) | (B << 16);
-
-		if(x > 1 && x < uniforms.width  - 2.0)
-		if(y > 1 && y < uniforms.height - 2.0){
-
-			for(int ox : {-2, -1, 0, 1, 2})
-			for(int oy : {-2, -1, 0, 1, 2}){
-				uint32_t pixelID = (x + ox) + int(uniforms.width) * (y + oy);
-				uint64_t udepth = *((uint32_t*)&depth);
-				uint64_t encoded = (udepth << 32) | color;
-
-				atomicMin(&framebuffer[pixelID], encoded);
-			}
-		}
-	});
-
-	grid.sync();
-
+	// draw some custom defined triangles
 	{
-		int numTriangles = 1;
-		int numVertices = 3 * numTriangles;
-		Triangles* triangles = allocator.alloc<Triangles*>(sizeof(Triangles) + sizeof(Point) * numVertices);
+		// make a ground plane
+		int cells = 20;
+		int numTriangles     = cells * cells * 2;
+		int numVertices      = 3 * numTriangles;
+		Triangles* triangles = allocator->alloc<Triangles*>(sizeof(Triangles));
+		triangles->positions = allocator->alloc<float3*  >(sizeof(float3) * numVertices);
+		triangles->uvs       = allocator->alloc<float2*  >(sizeof(float2) * numVertices);
+		triangles->colors    = allocator->alloc<uint32_t*>(sizeof(uint32_t) * numVertices);
 
 		triangles->count = numVertices;
 		triangles->instanceCount = 1;
+		
+		processRange(0, cells * cells, [&](int cellIndex){
+
+			int cx = cellIndex % cells;
+			int cy = cellIndex / cells;
+
+			float u0 = float(cx + 0) / float(cells);
+			float v0 = float(cy + 0) / float(cells);
+			float u1 = float(cx + 1) / float(cells);
+			float v1 = float(cy + 1) / float(cells);
+
+			int offset = 6 * cellIndex;
+
+			uint32_t color = 0;
+			uint8_t* rgb = (uint8_t*)&color;
+			rgb[0] = 255.0f * u0;
+			rgb[1] = 255.0f * v0;
+			rgb[2] = 0;
+
+			triangles->positions[offset + 0] = {2.0 * u0 - 1.0, -0.7, 2.0 * v0 - 1.0};
+			triangles->positions[offset + 2] = {2.0 * u1 - 1.0, -0.7, 2.0 * v0 - 1.0};
+			triangles->positions[offset + 1] = {2.0 * u1 - 1.0, -0.7, 2.0 * v1 - 1.0};
+			triangles->colors[offset + 0]    = color;
+			triangles->colors[offset + 2]    = color;
+			triangles->colors[offset + 1]    = color;
+
+			triangles->positions[offset + 3] = {2.0 * u0 - 1.0, -0.7, 2.0 * v0 - 1.0};
+			triangles->positions[offset + 5] = {2.0 * u1 - 1.0, -0.7, 2.0 * v1 - 1.0};
+			triangles->positions[offset + 4] = {2.0 * u0 - 1.0, -0.7, 2.0 * v1 - 1.0};
+			triangles->colors[offset + 3]    = color;
+			triangles->colors[offset + 5]    = color;
+			triangles->colors[offset + 4]    = color;
+		});
 
 		rasterizeTriangles(triangles, framebuffer);
+	}
+
+	{
+		Triangles* triangles = allocator->alloc<Triangles*>(sizeof(Triangles));
+		triangles->count = numTriangles * 3;
+		triangles->instanceCount = 1;
+
+		triangles->positions = positions;
+		triangles->uvs = uvs;
+		triangles->colors = colors;
+
+		Texture texture;
+		texture.width  = 1024;
+		texture.height = 1024;
+		texture.data   = textureData;
+
+		rasterizeTriangles(triangles, framebuffer, &texture);
 	}
 
 	grid.sync();
